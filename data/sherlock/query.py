969c7815585a418c8b509c971474c5a93593e270
George Wood (he/him) <60616269+gwo0d@users.noreply.github.com>
2022-11-19 17:55:03 +0000

    Convert query.py to OOP. - Made SherlockQuery class

---
+++ data/sherlock/query.py
@@ -0,0 +1,57 @@
+import os
+
+
+class SherlockQuery:
+    # TODO: Date searches (somehow) to allow for data pruning if storage capacity runs low.
+    __OUTPUT_DIRECTORY = "searches"
+    __results = None
+
+    def __init__(self, usernames=None):
+        self.__usernames = usernames
+
+    def query(self):
+        """
+        :return: A list of dictionaries containing two
+        keys, `user` and `hits` with the following format: x = [{user: <user>, hits: [<hit0>, <hit1>, ...]},
+        ...]. Returns None if `usernames` is empty.
+        """
+        if self.__usernames is not None:
+            users = ""
+            for user in self.__usernames:
+                users += user
+                users += " "
+
+            query_command = f"python3 sherlock/sherlock --csv --folderoutput {self.__OUTPUT_DIRECTORY} {users}"
+            os.system(query_command)
+
+            for user in self.__usernames:
+                cleanup_command = f"rm {self.__OUTPUT_DIRECTORY}/{user}.csv"
+                os.system(cleanup_command)
+
+            results = self.__format_output()
+            self.__results = results
+            return results
+        return None
+
+    def __format_output(self):
+        """
+        :return: A list of dictionaries containing two
+        keys, `user` and `hits` with the following format: x = [{user: <user>, hits: [<hit0>, <hit1>, ...]},
+        ...]. Returns None if `usernames` is empty.
+        """
+        if self.__usernames is not None:
+            output = []
+            for user in self.__usernames:
+                tmp = {"user": user}
+                with open(f"{user}.txt", "r") as file:
+                    tmp["hits"] = [x.strip("\n") for x in file.readlines()]
+                output.append(tmp)
+
+            return output
+        return None
+
+    def set_usernames(self, new_usernames):
+        self.__usernames = new_usernames
+
+    def get_results(self):
+        return self.__results
