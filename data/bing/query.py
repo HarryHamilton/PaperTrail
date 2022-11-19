ed7fd2ee5096273a5b6d42745a0785c63e505ff5
Luke Briggs <lukebriggs02@gmail.com>
2022-11-19 21:28:55 +0000

    Add query generation

--- data/bing/query.py
+++ data/bing/query.py
@@ -1,6 +1,8 @@
 import os
 import requests
-
+import typing
+from src.models import Individual
+import urllib.parse
 
 class BingQuery:
 
@@ -11,3 +13,17 @@ class BingQuery:
         print("API Key Unavailable")
 
     SEARCH_URL = "https://api.bing.microsoft.com/v7.0/search"
+
+    def __init__(self, individual: Individual):
+        self.individual = individual
+
+    def __generate_queries(self):
+        organisation_query = f"\"{self.individual.name}\" and "
+        organisations = [f"\"{organisation}\"" for organisation in self.individual.organisations]
+        organisation_query += f"({organisations})"
+
+        personal_site_query = " and ".join([f"\"{site}\"" for site in self.individual.domains])
+
+        query = f"({personal_site_query}) or ({organisation_query})"
+        return urllib.parse.quote_plus(query)
+
